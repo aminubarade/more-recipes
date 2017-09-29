b53eeba27c42b0a2ce6ddabb7a7c56867a51e536
aminubarade <aminubaradepage@gmail.com>
2017-09-29 12:01:59 +0100

    Added dummy pages to test the route url

--- server/more-recipes.js
+++ server/more-recipes.js
@@ -1,27 +1,32 @@
 const express = require('express');
-const moreRecipes = express();
+const app = express();
 const bodyParser = require("body-parser");
 
     app.use(bodyParser.urlencoded({extended:true}));
     app.set("view engine", "ejs");
 
-    //
-    moreRecipes.post('/api/recipes', function(res, req){
-        res.render();
+        //An API route that allows a user add a recipe:
+    app.post("/api/recipes", function(req, res){
+        //console.log(req.body);
+        res.render("viewrecipes");
     });
-    //
-    moreRecipes.put('/api/recipes/:recipeId', function(res, req){
-        res.send();
+    //An API route that allows a user modify a recipe
+    app.put("/api/recipes/:recipeId", function(req, res){
+        res.send("put route");
     });
-    moreRecipes.delete('/api/recipes/:recipeId', function(res, req){
-        res.send();
+    //An API route that allows a user to delete a recipe
+    app.delete("/api/recipes/:recipeId", function(req, res){
+            res.send("delete route");
+    });      
+    //An API route that allows a user post a review for a recipe
+    app.get("/api/recipes/", function(req, res){
+        res.render("addrecipe");
     });
-    moreRecipes.get('/api/recipes/', function(res, req){
-        res.render();
+    //An API route that allows a user to get just recipes with the most upvotes
+    app.post("/api/recipes/:recipeId/reviews", function(req, res){
+    res.send("reviews route");
     });
-    moreRecipes.post('/api/recipes/:recipeId/reviews', function(res, req){
-        res.send();
-    });
+
 
 app.listen(3000, function(){
     console.log("Server is up and running");
